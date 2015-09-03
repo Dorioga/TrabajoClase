a6ee5fa330d3e3e7e55bad01dd55a2ad304600d4
Dorioga <dhag100993@hotmail.com>
2015-09-03 09:33:01 -0500

    Trabajo 01
    Ejercicio de la clase del dia 03/09/2015.

---
+++ 0.0/Operaciones/Operaciones/IService1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Operaciones
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string Suma(int value, int value2);
+        [OperationContract]
+        string Resta(int value,int value2);
+        [OperationContract]
+        string Multiplicacion(int value, int value2);
+        [OperationContract]
+        string Division(float value, float value2);
+
+        
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    
+}
---
+++ 0.0/Operaciones/Operaciones/Service1.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Operaciones
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+
+
+
+        public string Suma(int value, int value2)
+        {
+            return (value + value2).ToString();
+        }
+
+        public string Resta(int value, int value2)
+        {
+            return (value - value2).ToString();
+        }
+
+        public string Multiplicacion(int value, int value2)
+        {
+            return (value * value2).ToString();
+        }
+
+        public string Division(float value, float value2)
+        {
+            if(value2 !=0){
+                return (value / value2).ToString();
+            }
+            else
+            {
+                return ("Indefinida");
+            }
+            
+        }
+    }
+}
